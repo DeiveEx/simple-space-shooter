a7cac4aa38c4155fd71512011360357eb9363f17
Deive_Ex <feliperobos@gmail.com>
2025-09-10 02:21:22 -0300

    GunController & Projectile

---
+++ Assets/Code/IGunController.cs
@@ -0,0 +1,4 @@
+﻿public interface IGunController
+{
+    void Shoot();
+}
---
+++ Assets/Code/Projectile.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(IMovementController))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _lifetime = 1;
+
+    private Action _onLifetimeEnd;
+    
+    public void Setup(Action onLifetimeEnd)
+    {
+        _onLifetimeEnd = onLifetimeEnd;
+    }
+    
+    private void OnEnable()
+    {
+        Invoke(nameof(OnLifetimeEnd), _lifetime);
+    }
+
+    private void OnLifetimeEnd()
+    {
+        _onLifetimeEnd?.Invoke();
+    }
+}
